303a4b9e858c517ad2871fa88592b93436bd5d3e
cailinkless <cailin.nysff@gmail.com>
2020-09-16 17:09:39 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(i)
+    if i <= 1
+      false
+    elsif i == 2
+      true
+    elsif i == 3
+      true
+    elsif i.even?
+      false
+    elsif (3..i-1).any? {|n| i % n == 0}
+      false
+    else
+      true
+    end
+  end
